c0a2e01ea492fa780547b96f189fbf674bdd6613
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-03-05 19:47:52 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,21 +1,13 @@
-// Add imports above this line
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-
-// Change code below this line
-
-
-
-import SimpleLightbox from "simplelightbox";
-
-
 
 const galleryEl = document.querySelector('.gallery');
 
 const gallery = galleryItems
   .map(({ preview, description, original }) => {
-    return `<li class="gallery__item"><a class="gallery__link" href="${original}">
-    <img class="gallery__image" src="${preview}" alt="${description}"></a></li>`;
+    return `<a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}"></a>`;
   })
   .join('');
 
@@ -27,4 +19,3 @@ const lightbox = new SimpleLightbox('.gallery a', {
 });
 
 console.log(galleryItems);
-
