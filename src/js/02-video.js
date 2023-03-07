c883805632b120c7ad443dd92f88f297260a99d1
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-03-07 22:04:22 +0200

    f

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,12 +5,8 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const CURRENT_TIME = 'videoplayer-current-time';
 
-
 const currentaTime = function (data) {
-  localStorage.setItem(
-    CURRENT_TIME,
-    JSON.stringify(data.seconds)
-  );
+  localStorage.setItem(CURRENT_TIME, JSON.stringify(data.seconds));
 
   let time = localStorage.getItem(CURRENT_TIME);
   console.log(time);
@@ -18,6 +14,4 @@ const currentaTime = function (data) {
 
 player.on('timeupdate', throttle(currentaTime, 1000));
 
-player
-  .setCurrentTime(localStorage.getItem(CURRENT_TIME));
-
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
