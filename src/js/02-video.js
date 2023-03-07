28d4d073c106ba89258867bd1e1d5e89b33a8fb0
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-03-07 22:02:49 +0200

    dfb

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,28 +3,21 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const CURRENT_TIME = 'videoplayer-current-time';
 
 
 const currentaTime = function (data) {
   localStorage.setItem(
-    'videoplayer-current-time',
+    CURRENT_TIME,
     JSON.stringify(data.seconds)
   );
 
-  let time = localStorage.getItem('videoplayer-current-time');
+  let time = localStorage.getItem(CURRENT_TIME);
   console.log(time);
 };
 
 player.on('timeupdate', throttle(currentaTime, 1000));
 
 player
-  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-  .then(function (seconds) {})
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        break;
-      default:
-        break;
-    }
-  });
+  .setCurrentTime(localStorage.getItem(CURRENT_TIME));
+
